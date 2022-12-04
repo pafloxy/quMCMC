68dd3f0685198c8ac030a0c4936a01a8062d36af
Neelkanth Rawat <42956384+neelkanthrawat@users.noreply.github.com>
2022-12-04 13:55:29 +0530

    add file calculations_from_sample.py

---
+++ qumcmc/calculations_from_samples.py
@@ -0,0 +1,67 @@
+from qumcmc.prob_dist import *
+
+
+class calns_from_samples:
+    '''  
+    A class to use list_of_samples for different caln. 
+    
+    Method given here (but not limited to) can
+    be come in handy for calculations of interest.
+    '''
+    def __init__(self, list_of_samples_sampled:list, num_mcmc_steps:int):
+        self.list_samples=list_of_samples_sampled
+        self.num_mcmc_steps=num_mcmc_steps
+        self.num_spins=len(list_of_samples_sampled[0])
+        self.all_poss_samples=states(num_spins=len(list_of_samples_sampled[0]))
+        # import states function from basic utils.py
+        # from qumcmc.prob_dist import *
+        self.dict_count=self.count_states_occurence(list_samples=self.list_samples)
+        self.dict_distn=self.empirical_distn(list_samples=self.list_samples)# not efficient! that is why I dont want inplace replacement
+    
+    def count_states_occurence(self,list_samples)->DiscreteProbabilityDistribution:
+        ''' 
+        Function to get dict of occurence count of sample
+
+        Returns: instance of DiscreteProbabilityDistrubution
+        '''
+        dict_count=DiscreteProbabilityDistribution(
+            dict(zip(self.all_poss_samples,[0]*(len(self.all_poss_samples))))
+        )
+        dict_count.update(dict(Counter(list_samples)))
+        return dict_count
+    
+    def empirical_distn(self, list_samples)->DiscreteProbabilityDistribution:
+        ''' 
+        Function to get dict of empirical distn from list of samples M.Chain was in.
+
+        Returns: an instance of DiscreteProbabilityDistrubution
+        '''
+        dict_distn=DiscreteProbabilityDistribution(
+            dict(zip(self.all_poss_samples,[0]*(len(self.all_poss_samples))))
+        )
+        update_with=DiscreteProbabilityDistribution(dict(Counter(list_samples)))
+        update_with.normalise()
+        dict_distn.update(update_with)
+        return dict_distn
+    
+    def running_avg_magnetization_as_list(self)->np.array:
+        """
+        Function to calculate the running average magnetization for the given mcmc trajectory as list
+        
+        Args:
+        list_states_mcmc= List of state markov chain is in after each MCMC step
+        
+        Returns: numpy array of running value of magnetization
+
+        """
+        list_of_strings = self.list_samples
+        list_of_lists = (
+            np.array([list(int(s) for s in bitstring) for bitstring in list_of_strings]) * 2
+            - 1
+        )
+        return np.array(
+            [
+                np.mean(np.sum(list_of_lists, axis=1)[:ii])
+                for ii in range(1, len(self.list_samples) + 1)
+            ]
+        )
