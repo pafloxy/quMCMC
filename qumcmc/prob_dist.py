4fcacf8fc57f22a34b568ac1e46f2e12a1528673
pafloxy <61856561+pafloxy@users.noreply.github.com>
2023-01-15 17:42:16 +0530

    modify kl-div

--- qumcmc/prob_dist.py
+++ qumcmc/prob_dist.py
@@ -111,7 +111,7 @@ class DiscreteProbabilityDistribution(dict):
 #1. KL divergence
 #2. JS divergence
 
-def kl_divergence(dict_p: dict, dict_q: dict):
+def kl_divergence(dict_p: dict, dict_q: dict, prelim_check: bool= False):
     ''' 
     Returns KL divergence KL(p||q);
 
@@ -126,25 +126,27 @@ def kl_divergence(dict_p: dict, dict_q: dict):
     dict_p and dict_q have same keys and that both the distributions are
     normalised then user can set it to 'False'.
     '''
+    ## mine ##
     # if prelim_check:
     #     #check for whether or not dict_p and dict_q have same keys
-    #     keys_p,keys_q=list(dict_p.keys()),list(dict_q.keys())# reason why not inplace
+    #     keys_p,keys_q=list(dict_p.keys()),list(dict_q.keys())
     #     keys_p.sort();keys_q.sort()
-    #     assert keys_p==keys_q, "keys of both the dictionaries dont match!"
+    #     if keys_p==keys_q: pass  #"keys of both the dictionaries dont match!"
+    #     else :
+    #         for key in set(dict_p.keys()).union(set(dict_q.keys())) :
+    #             if key not in dict_p.keys(): dict_p[key] = 0.0
+    #             if key not in dict_q.keys(): dict_q[key] = 0.0
 
+    #     # print('p: ', dict_p)
+    #     # print('q: ', dict_q)
     #     # check for whether values add to 1.
     #     eps=1e-6
     #     sum_vals_p=np.sum(list(dict_p.values()))
     #     assert np.abs(sum_vals_p-1.0)<=eps, "sum of values of dict_p must be 1."
     #     sum_vals_q=np.sum(list(dict_q.values()))
     #     assert np.abs(sum_vals_q-1.0)<=eps, "sum of values of dict_q must be 1."
-    
-    # #prep for caln
-    # p=DiscreteProbabilityDistribution(dict_p).index_sorted_dict()
-    # q=DiscreteProbabilityDistribution(dict_q).index_sorted_dict()
-    # p_arr,q_arr=np.array(list(p.values())).reshape((len(p))), np.array(list(q.values())).reshape((len(q)))
-    # return np.sum(np.where(p_arr>10**-6,p_arr*np.log2(p_arr/q_arr),0.))
 
+    ## Manuel's ##
     KL = 0
     for bitstring, p_data in dict_p.items():
         if bitstring in dict_q.keys():
@@ -173,14 +175,21 @@ def js_divergence(dict_p:dict,dict_q:dict, prelim_check=True):
         #check for whether or not dict_p and dict_q have same keys
         keys_p,keys_q=list(dict_p.keys()),list(dict_q.keys())
         keys_p.sort();keys_q.sort()
-        assert keys_p==keys_q, "keys of both the dictionaries dont match!"
-        
+        if keys_p==keys_q: pass  #"keys of both the dictionaries dont match!"
+        else :
+            for key in set(dict_p.keys()).union(set(dict_q.keys())) :
+                if key not in dict_p.keys(): dict_p[key] = 0.0
+                if key not in dict_q.keys(): dict_q[key] = 0.0
+
+        # print('p: ', dict_p)
+        # print('q: ', dict_q)
         # check for whether values add to 1.
         eps=1e-6
         sum_vals_p=np.sum(list(dict_p.values()))
         assert np.abs(sum_vals_p-1.0)<=eps, "sum of values of dict_p must be 1."
         sum_vals_q=np.sum(list(dict_q.values()))
         assert np.abs(sum_vals_q-1.0)<=eps, "sum of values of dict_q must be 1."
+
 
     #prep for caln
     p=DiscreteProbabilityDistribution(dict_p).index_sorted_dict()
