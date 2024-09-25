1e1270af75410af818b3874783d4bb11c62a214d
MSRudolph <manuel.rudolph@web.de>
2024-09-25 10:41:51 +0200

    fixing expectation values by using the actual MCMC chain not just the accepted transitions

--- qumcmc/basic_utils.py
+++ qumcmc/basic_utils.py
@@ -403,12 +403,13 @@ def get_random_state(num_spins: int) -> str:
     bin_next_state = f"{next_state:0{num_spins}b}"
     return bin_next_state
 
+
 def observable_expectation(
     observable: callable, mcmc_chain: MCMCChain, skip_init: int = 100
 ):
 
     sample_observable = []
-    for s in mcmc_chain.accepted_states:
+    for s in mcmc_chain.markov_chain:
 
         sample_observable.append(observable(s))
 
